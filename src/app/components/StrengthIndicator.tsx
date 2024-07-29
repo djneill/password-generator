c69f228dee6eca738d5d14f186f13c08e9cb414c
DJ Neill <dj.neill856@gmail.com>
2024-07-29 15:37:39 -0500

    adjust strenth indicator to use points for measurement instead of if statement

--- src/app/components/StrengthIndicator.tsx
+++ src/app/components/StrengthIndicator.tsx
@@ -1,20 +1,45 @@
 interface StrengthIndicatorProps {
-    strength: string;
+    strength: 'TOO WEAK!' | 'WEAK' | 'MEDIUM' | 'STRONG';
 }
 
 export default function StrengthIndicator({ strength }: StrengthIndicatorProps) {
+    const getStrengthColor = () => {
+        switch (strength) {
+            case 'TOO WEAK!': return 'bg-[#F64A4A]';
+            case 'WEAK': return 'bg-[#FB7C58]';
+            case 'MEDIUM': return 'bg-[#F8CD65]';
+            case 'STRONG': return 'bg-[#A4FFAF]';
+            default: return 'bg-[#817D92]';
+        }
+    };
+
+    const getStrengthBars = () => {
+        switch (strength) {
+            case 'TOO WEAK!': return 1;
+            case 'WEAK': return 2;
+            case 'MEDIUM': return 3;
+            case 'STRONG': return 4;
+            default: return 0;
+        }
+    };
+
     return (
-        <div className="mb-4">
-            <span>Strength: {strength}</span>
-            <div className="w-full bg-gray-700 h-2 mt-2">
-                <div
-                    className={`h-full ${strength === 'Weak' ? 'w-1/3 bg-red-500' :
-                        strength === 'Medium' ? 'w-2/3 bg-yellow-500' :
-                            'w-full bg-green-500'
-                        }`}
-                ></div>
+        <div className="bg-[#18171F] p-4 flex items-center justify-between font-jetbrains my-6">
+            <span className="text-gray-500 uppercase">Strength</span>
+            <div className="flex items-center space-x-4">
+                <span className="text-white uppercase font-bold">{strength}</span>
+                <div className="flex space-x-1">
+                    {[...Array(4)].map((_, index) => (
+                        <div
+                            key={index}
+                            className={`w-2 h-6 border-2 ${index < getStrengthBars()
+                                ? getStrengthColor()
+                                : 'border-white bg-transparent'
+                                }`}
+                        ></div>
+                    ))}
+                </div>
             </div>
         </div>
     );
 };
-
